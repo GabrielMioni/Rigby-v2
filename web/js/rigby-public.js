768c9a396290bb0ec4bc3b0c7f3afd5cf94d40c2
GabrielMioni <gabriel.mioni@gmail.com>
2018-03-12 21:43:27 -0700

    Added assetts for the public review form.

---
+++ web/js/rigby-public.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function () {
+    $(document).on('mouseover', '.star', function () {
+
+        let isFull = $(this).hasClass('star-full');
+
+        if (isFull === false)
+        {
+
+        }
+    })
+});
