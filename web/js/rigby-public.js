b02e01c9aa60d85a9c6760e7b4e663fc32c061f1
GabrielMioni <gabriel.mioni@gmail.com>
2018-03-13 17:43:02 -0700

    Added stateful star review hover/click.

--- web/js/rigby-public.js
+++ web/js/rigby-public.js
@@ -1,12 +1,97 @@
+class Stars {
+
+    constructor() {
+        this.starState = [0,0,0,0,0];
+        this.ratingSelect = $(document).find('.rating-select');
+        this.stars = this.ratingSelect.children();
+
+        this.starClick();
+        this.starHover();
+        this.starReset();
+    }
+
+    starClick() {
+
+        let self = this;
+
+        $(document).on('click', '.star', function () {
+
+            let prev = $(this).prevAll();
+            let starIndex = prev.length +1;
+            let newState = [];
+
+            while (newState.length < starIndex) {
+                newState.push(1);
+            }
+            while (newState.length < 5) {
+                newState.push(0);
+            }
+
+            if (newState !== self.state)
+            {
+                self.starState = newState;
+                self.evaluateState();
+            }
+        });
+    }
+
+    starHover() {
+
+        let stars = this.stars;
+
+        $(stars).on('mouseover', function () {
+
+            let previous = $(this).prevAll().addBack();
+            let next = $(this).nextAll();
+
+            previous.each(function () {
+
+                if (! $(this).hasClass('star-full'))
+                {
+                    $(this).addClass('star-full');
+                }
+            });
+
+            next.each(function () {
+                if ( $(this).hasClass('star-full'))
+                {
+                    $(this).removeClass('star-full');
+                }
+            })
+        })
+    }
+
+    starReset() {
+
+        let self = this;
+
+        $(this.ratingSelect).on('mouseleave', function () {
+            self.evaluateState();
+        });
+    }
+
+    evaluateState() {
+
+        let stars = this.stars;
+
+        $.each(this.starState, function (key, value) {
+
+            let starCurrent = stars[key];
+            let starFull = $(starCurrent).hasClass('star-full');
+
+            if (value === 0 && starFull === true)
+            {
+                $(starCurrent).removeClass('star-full');
+            }
+            if (value === 1 && starFull === false)
+            {
+                $(starCurrent).addClass('star-full');
+            }
+        });
+    }
+}
+
 
 $(document).ready(function () {
-    $(document).on('mouseover', '.star', function () {
-
-        let isFull = $(this).hasClass('star-full');
-
-        if (isFull === false)
-        {
-
-        }
-    })
+    new Stars();
 });
